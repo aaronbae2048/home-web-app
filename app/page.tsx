26c326e47d648ac7c5746862169eb7ca1f3fc3e7
Aaron Bae <aaronbae.personal@gmail.com>
2025-09-19 16:24:55 -0700

    starting a nextjs app

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// If I need to style this page, add the line
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>Welcome to AaronBae.com. This website is currently in progress.</main>
+  );
+}
